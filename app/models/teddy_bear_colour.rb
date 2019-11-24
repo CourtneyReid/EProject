2ce9c99ddc9bdaa245a3239492ff3f361e5de62d
CourtneyReid <Courtney.L.Reid@hotmail.com>
2019-11-24 17:28:21 -0600

    Added validation for the TeddyBearColour model.

--- app/models/teddy_bear_colour.rb
+++ app/models/teddy_bear_colour.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class TeddyBearColour < ApplicationRecord
   belongs_to :teddybear
   belongs_to :colour
+
+  validates :teddybear, :colour, presence: true, allow_blank: false
 end
