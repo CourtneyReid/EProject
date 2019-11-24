711f2487e3031aed47c498f9f0254fba5b38902b
CourtneyReid <Courtney.L.Reid@hotmail.com>
2019-11-24 17:17:59 -0600

    Added the Teddy Bear model with the proper references.

---
+++ app/models/teddy_bear.rb
@@ -0,0 +1,4 @@
+class TeddyBear < ApplicationRecord
+  belongs_to :size
+  belongs_to :status
+end
---
+++ db/migrate/20191124231713_create_teddy_bears.rb
@@ -0,0 +1,14 @@
+class CreateTeddyBears < ActiveRecord::Migration[6.0]
+  def change
+    create_table :teddy_bears do |t|
+      t.string :name
+      t.string :colour
+      t.string :description
+      t.integer :cost
+      t.references :size, null: false, foreign_key: true
+      t.references :status, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
