e1ecb758f7a229ea87b2cd1d7b1d86419c6b8d8d
CourtneyReid <Courtney.L.Reid@hotmail.com>
2019-11-24 16:53:17 -0600

    Added validations to the order model.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Order < ApplicationRecord
+  validates :subtotal, :taxes, :total, presence: true
+  validates :subtotal, :taxes, :total, numericality: { only_integer: true }
 end
