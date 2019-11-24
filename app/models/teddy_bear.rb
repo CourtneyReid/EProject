07d8ce7cc00673e7951a1d526e6d14469849231e
CourtneyReid <Courtney.L.Reid@hotmail.com>
2019-11-24 17:17:05 -0600

    Removed the Teddy Bear model because I didn't put the references in properly.

--- app/models/teddy_bear.rb
+++
@@ -1,2 +0,0 @@
-class TeddyBear < ApplicationRecord
-end
--- test/models/teddy_bear_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TeddyBearTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
