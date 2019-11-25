16492f1e89772f451793c04626c7fe608fad201f
CourtneyReid <Courtney.L.Reid@hotmail.com>
2019-11-24 19:26:29 -0600

    Added the columns to migrate to the migration file (Cost and Quantity).

--- db/migrate/20191125012205_add_cost_and_quantity_to_teddy_bear_orders.rb
+++ db/migrate/20191125012205_add_cost_and_quantity_to_teddy_bear_orders.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class AddCostAndQuantityToTeddyBearOrders < ActiveRecord::Migration[6.0]
   def change
+    add_column :teddybearorders, :cost, :integer
+    add_column :teddybearorders, :quantity, :integer
   end
 end
