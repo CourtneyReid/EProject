6d14dc520a1fbca1fd5d69d0d07a0c56d5aae331
CourtneyReid <Courtney.L.Reid@hotmail.com>
2019-11-24 20:48:40 -0600

    Added more routing for teddybears show.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/teddybears', to: 'teddybears#index'
+  get '/teddybears/:id', to: 'teddybears#show', id: /\d+/
 
   root to: 'teddybears#index'
 end
