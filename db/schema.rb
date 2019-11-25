ce5e6d5c9407d93502e2db76f2a16152114e00e7
CourtneyReid <Courtney.L.Reid@hotmail.com>
2019-11-24 19:24:14 -0600

    Migrated the new migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_232925) do
+ActiveRecord::Schema.define(version: 2019_11_25_012205) do
 
   create_table "colours", force: :cascade do |t|
     t.string "name"
