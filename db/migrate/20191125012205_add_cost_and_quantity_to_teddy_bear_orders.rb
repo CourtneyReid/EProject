8c0f9f01db0bc51a52f435733652c11498bd2674
CourtneyReid <Courtney.L.Reid@hotmail.com>
2019-11-24 19:34:53 -0600

    Deleted migration for adding cost and quantity.

--- db/migrate/20191125012205_add_cost_and_quantity_to_teddy_bear_orders.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddCostAndQuantityToTeddyBearOrders < ActiveRecord::Migration[6.0]
-  def change
-    add_column :teddybearorders, :cost, :integer
-    add_column :teddybearorders, :quantity, :integer
-  end
-end
