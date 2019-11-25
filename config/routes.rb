87d51bd42a8a4991cc732857d8bdf76a960d62df
CourtneyReid <Courtney.L.Reid@hotmail.com>
2019-11-24 20:43:16 -0600

    Added routing for teddybears controller/views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'teddybears/index'
   get 'teddybears/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/teddybears', to: 'teddybears#index'
+
+  root to: 'teddybears#index'
 end
