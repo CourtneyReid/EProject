c333e583db5d68ba68b2c5836d1ac573181ead37
CourtneyReid <Courtney.L.Reid@hotmail.com>
2019-11-25 18:42:08 -0600

    Generated all the active admin resources for the models.

---
+++ app/admin/teddy_bears.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register TeddyBear do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :colour_id, :description, :cost, :size_id, :status_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :colour_id, :description, :cost, :size_id, :status_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
